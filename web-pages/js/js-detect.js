3a6b15f90e7af74d0028bc95b4f887281ec658a8
delphidabbler <delphidabbler@gmail.com>
2020-06-10 04:38:16 +0100

    Delete redundant JS files and an associated CSS file.
    Removed all links to the files from HTML <head> sections

--- web-pages/js/js-detect.js
+++
@@ -1,9 +0,0 @@
-/*
-  js-detect.js
-
-  Code that scans an HTML document and shows elements with js-reveal-* class
-  names and hides elements with js-hide class names.
-
-  ALL CODE REMOVED. TODO: Delete this file once it is removed from all page 
-  head sections
-*/
